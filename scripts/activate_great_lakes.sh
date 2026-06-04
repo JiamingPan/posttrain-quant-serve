2a69e3a80cd0b7762c7735c6bd296725567cf067
Jiaming Pan <jiamingp@umich.edu>
2026-06-03 22:29:15 -0400

    Guard serving dependency install cache

--- scripts/activate_great_lakes.sh
+++ scripts/activate_great_lakes.sh
@@ -14,10 +14,18 @@ export PQS_ROOT="${PQS_ROOT:-$DEFAULT_PQS_ROOT}"
 export HF_HOME="${HF_HOME:-$PQS_ROOT/hf_cache}"
 export HF_DATASETS_CACHE="${HF_DATASETS_CACHE:-$PQS_ROOT/hf_datasets}"
 export PIP_CACHE_DIR="${PIP_CACHE_DIR:-$PQS_ROOT/pip-cache}"
+export CARGO_HOME="${CARGO_HOME:-$PQS_ROOT/cargo-home}"
+export RUSTUP_HOME="${RUSTUP_HOME:-$PQS_ROOT/rustup-home}"
+export TMPDIR="${TMPDIR:-$PQS_ROOT/tmp}"
 export PYTHONNOUSERSITE=1
+
+mkdir -p "$HF_HOME" "$HF_DATASETS_CACHE" "$PIP_CACHE_DIR" "$CARGO_HOME" "$RUSTUP_HOME" "$TMPDIR"
 
 source "$PQS_ROOT/envs/posttrain-quant-serve/bin/activate"
 
 echo "PQS_ROOT=$PQS_ROOT"
 echo "HF_HOME=$HF_HOME"
+echo "PIP_CACHE_DIR=$PIP_CACHE_DIR"
+echo "CARGO_HOME=$CARGO_HOME"
+echo "TMPDIR=$TMPDIR"
 which python
